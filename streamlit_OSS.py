69600505528ccc2cab6c63fa77e2cd4061f5bb1c
MaJaeGeon <59637845+MaJaeGeon@users.noreply.github.com>
2023-11-15 10:56:51 +0900

    Update streamlit_OSS.py

--- streamlit_OSS.py
+++ streamlit_OSS.py
@@ -2,10 +2,9 @@ import streamlit as st
 
 OSS_Score = [80, 70, 55, 30, 3, 3, 1, 0]
 
-st.write("# OSS 중간 시험 결과")
-st.write(" 얘들아 공부")
-st.write("ㄹㄹ")
+st.write("# Student ID : 23114143")
+st.write("# Name : 마재건")
 
 OSS_Score
 
-st.bar_chart(OSS_Score)
+st.line_chart(OSS_Score)
